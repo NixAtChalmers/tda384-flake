5da7afb5621f465d02a4563f1869fb5bfabc8e45
Oli <oli@olillin.com>
2026-01-30 13:48:29 +0100

    Build for all systems

--- flake.nix
+++ flake.nix
@@ -9,25 +9,22 @@
     self,
     nixpkgs,
   }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    inherit (pkgs) lib stdenv;
-
+    inherit (nixpkgs) lib;
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
   in {
-    packages.${system} = {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
       tsim = pkgs.callPackage ./packages/tsim.nix {};
       tsim_extractor = pkgs.callPackage ./packages/tsim_extractor.nix {};
-    };
+    });
 
-    nixosModules.default = {
+    nixosModules.default = {pkgs, ...}: {
       environment.systemPackages = with pkgs; [
         erlang
         jdk25
-        self.packages.${system}.tsim
-        self.packages.${system}.tsim_extractor
+        self.packages.${pkgs.stdenv.hostPlatform.system}.tsim
+        self.packages.${pkgs.stdenv.hostPlatform.system}.tsim_extractor
       ];
     };
   };
